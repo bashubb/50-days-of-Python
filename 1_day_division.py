ad200d39376ce0824827f0f2f6f0262a0f8faa11
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-01-11 22:49:06 +0100

    First program - second commit

--- 1_day_division.py
+++ 1_day_division.py
@@ -1,14 +1,17 @@
+'''
+function called divide_or_square that takes one argument (a number) 
+and returns the square root of the number if it is divisible by 5, 
+or its remainder if it is not divisible by 5.
+'''
+
 import math
 
 def divide_or_square(number):
     if number % 5 == 0:
-        return math.sqrt(number)
+        return math.sqrt(number)    #returns the square root of the number
     else:
-        return number % 5 
+        return number % 5   #returns the remainder of the number
 
 
-print(divide_or_square(10))
-print(divide_or_square(12))
-print(divide_or_square(13))
-print(divide_or_square(15))
+
 
