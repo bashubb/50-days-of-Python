a47d492664c2a5fe3de397bbd42c943530ce8b6a
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-01-15 10:52:00 +0100

    Third program - second commit

--- day_3.py
+++ day_3.py
@@ -1,5 +1,10 @@
 # 3rd day
 # basic challange: register check
+'''
+function called register_check that checks how many students are in school. 
+The function takes a dictionary as a parameter. If the student is in school, 
+the dictionary says "yes." If the student is not in school, the dictionary says "no."
+'''
 
 def register_check(collection: dict):
     number_of_students = 0
@@ -10,10 +15,16 @@ def register_check(collection: dict):
 
 
 # extra challange: 
+'''
+Task is to write a code that will return a tuple of all the names in the list
+that have only lowercase letters. Your tuple should have names sorted alphabetically 
+in descending order. 
+'''
 
 
 def find_lowercase(collection: list):
     lower_case_list = [item for item in collection if item.islower()]
+    lower_case_list.sort(reverse= True)
     return tuple(lower_case_list)
 
 names = ["kerry", "dickson", "John", "Mary", 
