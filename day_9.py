53d1c4e2cdc4a464533b82a6aa3974d32387f989
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-01-22 20:36:30 +0100

    Ninth program - second commit

--- day_9.py
+++ day_9.py
@@ -1,5 +1,7 @@
 #day_9th:
 '''
+a function called biggest_odd that takes a string of 
+numbers and returns the biggest odd number in the list.
 '''
 
 
@@ -10,4 +12,34 @@ def biggest_odd(number_string: str):
 
     return f'The biggest number from {number_string} converted to the list and filtered to odd numbers only {odds} is {biggest}'  
 
-print(biggest_odd('23569'))
+#Extra challange:
+'''
+function called zeros_last. This function takes a list as an argument. 
+If a list has zeros (0), it will move them to the end of the list and maintain 
+the order of the other numbers in the list. If there are no zeros in the list, 
+the function should return the original list sorted in ascending order.
+'''
+
+def zeros_last(collection: list):
+    counter_of_zero = collection.count(0)
+    if 0 in collection:
+        for number in collection:
+            if number == 0:
+                collection.remove(number)
+        sorted_collection = sorted(collection)
+        for number in range(counter_of_zero):
+            sorted_collection.append(0)
+        return sorted_collection
+    else:
+        collection.sort()
+        return collection
+
+if __name__ == '__main__':
+
+    print(biggest_odd('23569'))
+
+    example_1 = [1, 4, 6, 0, 7, 0, 9]
+    example_2 = [2, 1, 4, 7, 6]
+
+    print(zeros_last(example_1))
+    print(zeros_last(example_2))
