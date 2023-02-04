d3d5447eb06abb5a2de497eb0187132f0c6caf21
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-02-04 11:23:51 +0100

    17th programm 3rd commit

--- day_17.py
+++ day_17.py
@@ -14,4 +14,33 @@ def user_name():
     return new_name
 
 
-print(user_name())
+
+#extra challange
+'''
+names = [ "Peter", "Jon", "Andrew"] 
+Write a function called sort_length that takes a list of strings as an argument 
+and sorts the strings in ascending order according to their length. For example,
+the list above should return: ['Jon', 'Peter', 'Andrew'] Do not use the built-in sort functions.
+'''
+
+def sort_length (collection: list):
+    for index in range(len(collection) - 1):
+        if len(collection[index]) > len(collection[index+1]):
+            a, b = index, index+1
+            collection[b], collection[a] = collection[a], collection[b]
+    return collection
+
+
+            
+
+
+
+
+if __name__ == '__main__':
+
+    #execution basic
+    print(user_name())
+
+    #execution extra
+    names = [ "Peter", "Jon", "Andrew"] 
+    print(sort_length(names))
