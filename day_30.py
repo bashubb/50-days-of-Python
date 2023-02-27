0d8fb9f6932c694c81bbf9a0e1a28f0bfb99fba3
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-02-27 19:29:50 +0100

    30th program - 3rd  commit

--- day_30.py
+++ day_30.py
@@ -10,8 +10,34 @@ def repeated_name(collection: list):
     return max(count_names.items())
 
 
+#extra:
+'''
+You work for a local school in your area. 
+The school has a list of names of students saved in a list. 
+The school has asked you to write a program that takes a list of names and sorts them alphabetically.
+The names should be sorted by last names. 
+Here is a list of names: ["Beyoncé Knowles", "Alicia Keys", "Katie Perry", "Chris Brown", "Tom Cruise”] 
+Your code should not just sort them alphabetically; it should also switch the names
+(the last name must be the first). 
+Here is how your code output should look: 
+['Brown Chris', 'Cruise Tom', 'Keys Alicia', 'Knowles Beyoncé', 'Perry Katie'] 
+Write a function called sorted_names
+'''
+
+def sorted_name(collection: list):
+    seperated_names = [name.split() for name in collection]
+    switched_names = [' '.join(item[::-1]) for item in seperated_names]
+    sorted_names = sorted(switched_names)
+    return  sorted_names
+
+
 if __name__ == '__main__':
     
     # execute basic:
     example = ["John", "Peter", "John", "Peter", "Jones", "Peter"]
-    print(repeated_name(example))
+    print(repeated_name(example))   #output : ('Peter', 3)
+
+    #execute extra:
+    example_names = ["Beyoncé Knowles", "Alicia Keys", "Katie Perry", "Chris Brown", "Tom Cruise"]
+    print(sorted_name(example_names))   #output : ['Brown Chris', 'Cruise Tom', 'Keys Alicia', 'Knowles Beyoncé', 'Perry Katie']
+
