0e6a821dbfbcc8466019a7239277493e88c6a1b4
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-02-27 19:00:51 +0100

    30th program - 2nd commit

--- day_30.py
+++ day_30.py
@@ -1,2 +1,17 @@
 #day30
-#basic
+#basic:
+'''
+repeated_name that finds the most repeated name in the following list. 
+name = ["John", "Peter", "John", "Peter", "Jones", "Peter"]
+'''
+
+def repeated_name(collection: list):
+    count_names = {name: collection.count(name) for name in collection}
+    return max(count_names.items())
+
+
+if __name__ == '__main__':
+    
+    # execute basic:
+    example = ["John", "Peter", "John", "Peter", "Jones", "Peter"]
+    print(repeated_name(example))
