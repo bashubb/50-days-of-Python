ce3242bc46c7e4189a2fdb565c27420ea8979151
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-03-07 20:32:46 +0100

    33rd extra program - commit 1st

---
+++ day_33_extra.py
@@ -0,0 +1,15 @@
+#extra:
+'''
+You want to implement a code that will search for a number in a range. 
+You have a decision to make as to whether to store the number in a set or a list. 
+Your decision will be based on time. You have to pick a data type that executes faster. 
+You have a range, and you can either store it in a set or a list, depending on which one executes 
+faster when you are searching for a number in the range. 
+See below: a = range(10000000) x = set(a) y = list(a) 
+Let’s say you are looking for the number 9999999 in the range above. 
+Search for this number in the list "x" and the set "y." Your challenge is to find which code executes faster.
+You will select the one that executes the fastest between lists and sets. Run the two searches and time them .
+'''
+
+def which_data_faster():
+    pass
